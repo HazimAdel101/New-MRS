5bdbe816ab4ba820e3acac1ebe873dfdf5b07669
HazimAdel101 <alsaqqafhazim2002@gmail.com>
2024-02-09 16:58:10 +0300

    finish filtering

--- movie/views.py
+++ movie/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+import os
+import json
+from django.shortcuts import render, get_object_or_404
 from .models import Movie, Genre
-
-# Create your views here.
 
 def movies(request):
     title = 'movies'
     movies = Movie.objects.all()
     genres = Genre.objects.all()
-    action_movies = Movie.objects.filter(genres__name='action')
-    return render(request, "movies.html", {'title': title, 'movies': movies, 'genres': genres, 'action_movies': action_movies})
+    
+    json_file_path = os.path.join(os.path.dirname(__file__), '..', 'cleaned_data2.json')
+    with open(json_file_path) as f:
+        data = json.load(f)[:149]
+    
+    return render(request, "movies.html", {'title': title, 'movies': movies, 'genres': genres, 'data': data})
 
 
 def top_movies(request):
@@ -18,5 +22,19 @@ def top_movies(request):
 
 def movie_details(request, id):
     title = 'Details'
-    movie = Movie.objects.get(id = id)
-    return render(request, 'movie-details.html', {'title': title, 'movie': movie})
+    
+    # movie = get_object_or_404(Movie, id=id)
+    
+    # movie.views += 1
+    # movie.save()
+    
+    
+     # Fetching movie details from JSON file
+    json_file_path = os.path.join(os.path.dirname(__file__), '..', 'cleaned_data2.json')
+    with open(json_file_path) as f:
+        data = json.load(f)
+        # Finding the movie details in the JSON data based on its ID
+        movie_data = next((m for m in data if m['id'] == id), None)
+
+    # movie = Movie.objects.get(id = id)
+    return render(request, 'movie-details.html', {'title': title, 'movie_data': movie_data,})
